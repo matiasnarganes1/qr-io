08c28f773af36af99b5d37fdc93f10c88e2109b1
Matias Narganes <97039933+matiasnarganes1@users.noreply.github.com>
2026-02-06 10:16:03 -0300

    favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { QrGeneratorComponent } from './components/qr-generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, QrGeneratorComponent],
+  imports: [QrGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
